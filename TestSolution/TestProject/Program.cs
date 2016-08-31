3e72ea94bdcea794f84d887302c0936ccbdcabf8
mraleigh <xxemptychickenx@hotmail.com>
2016-08-31 13:19:53 -0600

    Added the FINAL countdown.

---
+++ TestSolution/TestProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            while (x > 0)
+            {
+                Console.WriteLine(x--);
+            }
+            Console.WriteLine("Boom!!!");
+            Console.Read();
+        }
+    }
+}
